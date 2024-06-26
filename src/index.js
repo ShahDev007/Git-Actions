64cb48ef9428bcda9f33c691d939aba9cf249cb1
Dev Shah <dshah7971@sdsu.edu>
2024-06-26 15:26:45 -0700

    Added index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// src/index.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route handler for the default home page
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
